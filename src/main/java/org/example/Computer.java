7ef11ae450b0f474e9ab3b5ded2dde32df0b5949
pawl1n <pinchuk.andryusha310212@gmail.com>
2022-11-03 21:06:37 +0200

    feat: computer methods

--- src/main/java/org/example/Computer.java
+++ src/main/java/org/example/Computer.java
@@ -3,10 +3,11 @@ package org.example;
 import java.util.UUID;
 
 public class Computer {
+  private boolean running;
   private int ram;
   private int cores;
   private String name;
-  private UUID number;
+  private final UUID number;
   private OperatingSystem operatingSystem;
 
   public Computer(int ram, int cores, String name, OperatingSystem operatingSystem) {
@@ -15,34 +16,87 @@ public class Computer {
     this.name = name;
     this.operatingSystem = operatingSystem;
     this.number = UUID.randomUUID();
+    this.running = false;
   }
 
   public static ComputerBuilder builder() {
     return new ComputerBuilder();
   }
 
+  public void turnOn() {
+    running = true;
+  }
+
+  public void turnOff() {
+    running = false;
+  }
+
+  public boolean isRunning() {
+    return running;
+  }
+
+  public int getRam() {
+    return ram;
+  }
+
+  public int getCores() {
+    return cores;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public UUID getUUIDNumber() {
+    return number;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setRam(int ram) {
+    if (running) throw new IllegalArgumentException("Turn off pc first");
+
+    this.ram = ram;
+  }
+
+  public void setCores(int cores) {
+    if (running) throw new IllegalArgumentException("Turn off pc first");
+
+    this.cores = cores;
+  }
+
+  public OperatingSystem getOperatingSystem() {
+    return operatingSystem;
+  }
+
+  public void setOperatingSystem(OperatingSystem operatingSystem) {
+    this.operatingSystem = operatingSystem;
+  }
+
   @Override
   public String toString() {
     return "Computer{" +
-        "ram=" + ram +
-        ", cores=" + cores +
-        ", name='" + name + '\'' +
-        ", number=" + number +
-        ", operatingSystem=" + operatingSystem +
-        '}';
+            "running=" + running +
+            ", ram=" + ram +
+            ", cores=" + cores +
+            ", name='" + name + '\'' +
+            ", number=" + number +
+            ", operatingSystem=" + operatingSystem +
+            '}';
   }
 
   enum OperatingSystem{
-    Windows,
-    Linux,
-    MacOS
+    WINDOWS,
+    LINUX,
+    MACOS
   }
 
   public static class ComputerBuilder {
     private int ram;
     private int cores;
     private String name;
-    private UUID number;
     private OperatingSystem operatingSystem;
 
     public ComputerBuilder ram(int ram) {
@@ -57,11 +111,6 @@ public class Computer {
 
     public ComputerBuilder name(String name) {
       this.name = name;
-      return this;
-    }
-
-    public ComputerBuilder number() {
-      this.number = UUID.randomUUID();
       return this;
     }
 
